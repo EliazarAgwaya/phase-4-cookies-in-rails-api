b0ee555cb2efd5939ab56725321532087fa915a5
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-10-01 03:49:46 +0300

    commit tree 2b2da65091b47df2b00f875ba640e1b848476b1b

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Phase4CookiesInRailsApi
     config.api_only = true
 
     # Adding back cookies and session middleware
-    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Cookies 
     config.middleware.use ActionDispatch::Session::CookieStore
 
     # Use SameSite=Strict for all cookies to help protect against CSRF
